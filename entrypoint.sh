85ce023932f903de7b62f15d768936cb892e30ab
formica <andrea.formica@cern.ch>
2022-12-02 23:11:10 +0100

    fix: correct entrypoint.sh
    The config file location for starting spring boot was not found.
    Comment the Docker environment class in spring.factories to avoid calling it. The new entrypoint should not need it.

--- entrypoint.sh
+++ entrypoint.sh
@@ -70,7 +70,7 @@ print_application_properties >> ${app_properties} &
 echo "$USER is starting server with JAVA_OPTS : $JAVA_OPTS from user directory $PWD"
 if [ x"$1" = x"" ]; then
     echo "execute command ${prj_dir}/crest.jar"
-    exec java $JAVA_OPTS -jar ${prj_dir}/crest.jar --spring.config.location=optional:classpath:/,optional:classpath:/config/,file:${app_properties} 2>>/tmp/err.log
+    exec java $JAVA_OPTS -jar ${prj_dir}/crest.jar --spring.config.location=optional:classpath:/,optional:classpath:./config/,file:${app_properties} 2>>/tmp/err.log
 else
     sh -c "$@"
 fi
