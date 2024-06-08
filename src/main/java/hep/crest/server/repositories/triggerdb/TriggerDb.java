e0ee2716972a29a78d5ff9f1d2085316fb52b543
formica <andrea.formica@cern.ch>
2024-06-08 16:27:23 +0200

    improve query trigger codde

--- src/main/java/hep/crest/server/repositories/triggerdb/TriggerDb.java
+++ src/main/java/hep/crest/server/repositories/triggerdb/TriggerDb.java
@@ -7,11 +7,67 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import javax.sql.DataSource;
 import java.io.InputStream;
 import java.sql.Blob;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Slf4j
 public class TriggerDb implements ITriggerDb {
+
+    /**
+     * Query map.
+     */
+    private static final Map<String, String> QUERY_MAP = Map.of(
+            "L1PS", "select L1PS_DATA from %s.%s where l1ps_id=?",
+            "HLTM", "select HMT.HTM_DATA from %s.SUPER_MASTER_TABLE SMT, %s.%s HMT "
+                    + "where HMT.HTM_ID=SMT.SMT_HLT_MENU_ID and SMT.SMT_ID=?",
+            "L1M", "select L1MT.L1TM_DATA from %s.SUPER_MASTER_TABLE SMT, %s.%s L1MT "
+                   + "where L1MT.L1TM_ID=SMT.SMT_L1_MENU_ID and SMT.SMT_ID=?",
+            "HLTPS", "select HPS_DATA from %s.%s where hps_id=?",
+            "BG", "select L1BGS_DATA from %s.%s where l1bgs_id=?",
+            "MG", "select HMG.HMG_DATA from %s.SUPER_MASTER_TABLE SMT, %s.%s HMK, "
+                  + " %s.HLT_MENU_GROUP_KEY HMG "
+                  + " where HMG.HMG_IN_USE=1 and SMT.SMT_HLT_MENU_ID = HMG.HMG_HLT_MENU_ID "
+                  + " and SMT.SMT_ID=?",
+            "JO", "select JO.HJO_DATA from %s.SUPER_MASTER_TABLE SMT, %s.%s JO "
+                  + " where JO.HJO_ID=SMT.SMT_HLT_JOBOPTIONS_ID "
+                  + " and SMT.SMT_ID=?"
+    );
+
+    /**
+     * Schema map.
+     */
+    private static final Map<String, String> SCHEMA_MAP = Map.of(
+            "CONF_DATA_RUN3", "ATLAS_CONF_TRIGGER_RUN3",
+            "CONF_MC_RUN3", "ATLAS_CONF_TRIGGER_MC_RUN3",
+            "CONF_REPR_RUN3", "ATLAS_CONF_TRIGGER_REPR_RUN3"
+    );
+
+    /**
+     * Table map.
+     */
+    private static final Map<String, String> TABLE_MAP = Map.of(
+            "L1PS", "l1_prescale_set",
+            "HLTM", "hlt_menu",
+            "L1M", "l1_menu",
+            "HLTPS", "hlt_prescale_set",
+            "BG", "l1_bunch_group_set",
+            "MG", "hlt_monitoring_groups",
+            "JO", "hlt_joboptions"
+    );
+
+    /**
+     * Column name.
+     */
+    private static final Map<String, String> COLUMN_MAP = Map.of(
+            "L1PS", "L1PS_DATA",
+            "HLTM", "HTM_DATA",
+            "L1M", "L1TM_DATA",
+            "HLTPS", "HPS_DATA",
+            "BG", "L1BGS_DATA",
+            "MG", "HMG_DATA",
+            "JO", "HJO_DATA"
+    );
 
     /**
      * The JdbcTemplate.
@@ -35,7 +91,7 @@ public class TriggerDb implements ITriggerDb {
         String sql = buildSql(components);
         try {
             Long id = components.getId();
-            String columnName = getColumnName(components);
+            String columnName = COLUMN_MAP.get(components.getTable());
             log.debug("Execute query {} using {}", sql, id);
             return jdbcTemplate.queryForObject(sql, new Object[]{id}, (rs, rowNum) -> {
                 Blob blob = rs.getBlob(columnName);
@@ -44,7 +100,7 @@ public class TriggerDb implements ITriggerDb {
         }
         catch (EmptyResultDataAccessException e) {
             // No result, log the error.
-            log.error("Cannot find {} for ID {}: {}", components.getFullTable(),
+            log.error("Cannot find {} for ID {}: {}", components.getTable(),
                     components.getId(), e);
             return null;
         }
@@ -57,69 +113,23 @@ public class TriggerDb implements ITriggerDb {
      */
     protected String buildSql(UrlComponents components) {
         String schema = components.getSchema();
-        switch (components.getTable()) {
-            case "L1PSK":
-                return "select L1PS_DATA from "
-                       + schema + "." + components.getFullTable()
-                       + " where l1ps_id=?";
-            case "HLTMK":
-                return "select HMT.HTM_DATA from "
-                       + schema + ".SUPER_MASTER_TABLE SMT, "
-                       + schema + "." + components.getFullTable() + " HMT"
-                       + " where HMT.HTM_ID=SMT.SMT_HLT_MENU_ID and SMT.SMT_ID=?";
-            case "L1MK":
-                return "select L1MT.L1TM_DATA from "
-                       + schema + ".SUPER_MASTER_TABLE SMT, "
-                       + schema + "." + components.getFullTable() + " L1MT"
-                       + " where L1MT.L1TM_ID=SMT.SMT_L1_MENU_ID and SMT.SMT_ID=?";
-            case "HLTPSK":
-                return "select HPS_DATA from "
-                       + schema + "." + components.getFullTable()
-                       + " where HPS_ID=?";
-            case "BGK":
-                return "select L1BGS_DATA from "
-                       + schema + "." + components.getFullTable()
-                       + " where L1BGS_ID=?";
-            case "MGK":
-                return "select HMG.HMG_DATA from "
-                       + schema + ".SUPER_MASTER_TABLE SMT, "
-                       + schema + "." + components.getFullTable() + " HMG"
-                       + " where HMG.HMG_IN_USE=1 and SMT.SMT_HLT_MENU_ID = HMG.HMG_HLT_MENU_ID "
-                       + " and SMT.SMT_ID=?";
-            case "JOK":
-                return "select JO.HJO_DATA from "
-                       + schema + ".SUPER_MASTER_TABLE SMT, "
-                       + schema + "." + components.getFullTable() + " JO"
-                       + " where JO.HJO_ID=SMT.SMT_HLT_JOBOPTIONS_ID "
-                       + " and SMT.SMT_ID=?";
-            default:
-                throw new IllegalArgumentException("Unknown table: " + components.getTable());
+        if (SCHEMA_MAP.containsKey(schema)) {
+            schema = SCHEMA_MAP.get(schema);
         }
-    }
-
-    /**
-     * Get the column name.
-     * @param components
-     * @return String
-     */
-    protected String getColumnName(UrlComponents components) {
+        else {
+            throw new IllegalArgumentException("Unknown schema: " + schema);
+        }
+        if (!QUERY_MAP.containsKey(components.getTable())) {
+            throw new IllegalArgumentException("Unknown table: " + components.getTable());
+        }
+        String query = QUERY_MAP.get(components.getTable());
         switch (components.getTable()) {
-            case "L1PSK":
-                return "L1PS_DATA";
-            case "HLTMK":
-                return "HTM_DATA";
-            case "L1MK":
-                return "L1TM_DATA";
-            case "HLTPSK":
-                return "HPS_DATA";
-            case "BGK":
-                return "L1BGS_DATA";
-            case "MGK":
-                return "HMG_DATA";
-            case "JOK":
-                return "HJO_DATA";
+            case "L1PS":
+            case "HLTPS":
+            case "BG":
+                return String.format(query, schema, TABLE_MAP.get(components.getTable()));
             default:
-                throw new IllegalArgumentException("Unknown table: " + components.getTable());
+                return String.format(query, schema, schema, TABLE_MAP.get(components.getTable()));
         }
     }
 
--- src/main/java/hep/crest/server/repositories/triggerdb/UrlComponents.java
+++ src/main/java/hep/crest/server/repositories/triggerdb/UrlComponents.java
@@ -20,29 +20,4 @@ public class UrlComponents {
      */
     private final Long id;
 
-    /**
-     * Get the full table name from the alias.
-     *
-     * @return the full table name.
-     */
-    public String getFullTable() throws IllegalArgumentException {
-        switch (this.table) {
-            case "L1PSK":
-                return "l1_prescale_set";
-            case "HLTMK":
-                return "hlt_menu";
-            case "L1MK":
-                return "l1_menu";
-            case "HLTPSK":
-                return "hlt_prescale_set";
-            case "BGK":
-                return "l1_bunch_group_set";
-            case "MGK":
-                return "hlt_monitoring_groups";
-            case "JOK":
-                return "hlt_joboptions";
-            default:
-                throw new IllegalArgumentException("Unknown table: " + this.table);
-        }
-    }
 }
