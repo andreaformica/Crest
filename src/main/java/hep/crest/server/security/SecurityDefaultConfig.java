73d1ac7505251af14a63231905aa74f5485bc7d4
andrea formica <andrea.formica@cern.ch>
2024-06-28 10:30:49 +0200

    Fix deprecated in securityConfig. Add option to disable admin methods and insertion methods.

--- src/main/java/hep/crest/server/security/SecurityDefaultConfig.java
+++ src/main/java/hep/crest/server/security/SecurityDefaultConfig.java
@@ -1,16 +1,20 @@
 package hep.crest.server.security;
 
+import hep.crest.server.config.CrestProperties;
 import org.keycloak.representations.AccessToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.context.WebApplicationContext;
+
+import javax.ws.rs.HttpMethod;
 
 /**
  * Web security configuration. This is used only with profile different from keycloak.
@@ -22,19 +26,51 @@ import org.springframework.web.context.WebApplicationContext;
 @Profile({"!keycloak"})
 @Configuration
 @EnableWebSecurity
-public class SecurityDefaultConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // In this case everything is allowed.
-        http.authorizeRequests().antMatchers("/**").permitAll();
+public class SecurityDefaultConfig {
+
+    /**
+     * Properties.
+     */
+    @Autowired
+    private CrestProperties cprops;
+
+
+    /**
+     * Security filter chain.
+     * @param http
+     * @return SecurityFilterChain
+     * @throws Exception
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        if ("active".equals(cprops.getSecurity())) {
+            http
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.GET, "/**").permitAll()
+                    .antMatchers(HttpMethod.HEAD, "/**").permitAll()
+                    .anyRequest().denyAll(); // Deny all other methods (POST, DELETE, etc.)
+        }
+        else {
+            http.authorizeRequests().antMatchers("/**").permitAll();
+        }
         http.headers().frameOptions().disable();
         http.csrf().disable();
+
+        return http.build();
     }
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        // Ignore all urls.
-        web.ignoring().antMatchers("/**");
+    /**
+     * Web security customizer.
+     * @return WebSecurityCustomizer
+     */
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers(
+                "/static/**",
+                "/css/**",
+                "/js/**",
+                "/images/**"
+        );
     }
 
     @Bean
