015715869b20ddd93cba77b0684b0eb352b1575e
andrea formica <andrea.formica@cern.ch>
2024-12-05 15:29:18 +0100

    do not use redis for tests

--- src/test/java/hep/crest/server/TestEmbeddedRedisConfig.java
+++ src/test/java/hep/crest/server/TestEmbeddedRedisConfig.java
@@ -3,13 +3,17 @@ package hep.crest.server;
 import com.redis.testcontainers.RedisServer;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.test.annotation.IfProfileValue;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
+@Slf4j
 @TestConfiguration
-@Profile({"test-local"})
+@Profile({"testredis"})
+@IfProfileValue(name = "spring.profiles.active", value = "testredis")
 public class TestEmbeddedRedisConfig {
 
 
@@ -17,17 +21,21 @@ public class TestEmbeddedRedisConfig {
      * The redis server.
      */
     private RedisServer redisServer;
+    /**
+     * The redis container.
+     */
     private static final GenericContainer<?> redis =
-            new GenericContainer<>(DockerImageName.parse("redis:7.2")).withExposedPorts(6379);
+            new GenericContainer<>(DockerImageName.parse("redis:latest")).withExposedPorts(6380);
 
     @PostConstruct
     public void startRedis() {
+        log.info("Starting redis server...");
         redis.start();
     }
 
     @PreDestroy
     public void stopRedis() {
+        log.info("Stopping redis server...");
         redis.stop();
     }
-
 }
---
+++ src/test/java/hep/crest/server/TestRedisIntegration.java
@@ -0,0 +1,25 @@
+package hep.crest.server;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.annotation.IfProfileValue;
+
+@Profile("testredis") // Use a specific profile if needed
+@IfProfileValue(name = "spring.profiles.active", value = "testredis")
+@SpringBootTest
+public class TestRedisIntegration {
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    @Test
+    void testRedisFunctionality() {
+        redisTemplate.opsForValue().set("key", "value");
+        String value = redisTemplate.opsForValue().get("key");
+        Assertions.assertEquals("value", value);
+    }
+}
