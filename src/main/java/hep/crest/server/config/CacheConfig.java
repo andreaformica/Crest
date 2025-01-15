ab62b07aac00826968d2ed189f12732c411e7dfd
andrea formica <andrea.formica@cern.ch>
2025-01-15 18:39:50 +0100

    modify caching config

--- src/main/java/hep/crest/server/config/CacheConfig.java
+++ src/main/java/hep/crest/server/config/CacheConfig.java
@@ -1,11 +1,13 @@
 package hep.crest.server.config;
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableCaching
+@Slf4j
 public class CacheConfig {
 
     /**
@@ -19,6 +21,7 @@ public class CacheConfig {
      */
     @PostConstruct
     public void clearCachesOnStartup() {
+        log.info("Clearing all caches on startup...");
         cacheManager.getCacheNames().forEach(name -> cacheManager.getCache(name).clear());
     }
 }
